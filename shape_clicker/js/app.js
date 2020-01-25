aa3dd64bc8b8e3fae9218c20fe46b0075137bceb
Aarik Robles <roblesaarik@gmail.com>
2020-01-25 12:13:57 -0800

    Finished shape clicker

--- shape_clicker/js/app.js
+++ shape_clicker/js/app.js
@@ -1,3 +1,23 @@
-console.log('shape clicker js file is connected');
+console.log("shape clicker js file is connected");
 
 //remember to set up your window onload!
+const shapes = [
+  "circle",
+  "square",
+  "triangle-down",
+  "octagon",
+  "heart",
+  "triangle",
+];
+
+$(() => {
+  const handleClick = event => {
+    let $currentShape = $(event.currentTarget);
+    $currentShape.removeClass();
+    $currentShape.addClass(shapes[0]);
+    shapes.push(shapes[0]);
+    shapes.shift([0]);
+  };
+
+  $(".triangle").on("click", handleClick);
+});
