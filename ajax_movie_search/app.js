7c884a3ef10e476057b8b2f7b6dffba0093d9478
Aarik Robles <roblesaarik@gmail.com>
2020-01-28 18:39:33 -0800

    created a search bar using an API

--- ajax_movie_search/app.js
+++ ajax_movie_search/app.js
@@ -1,16 +1,22 @@
 console.log("Works");
 
 $(() => {
-  const promise = $.ajax({
-    url: "http://www.omdbapi.com/?apikey=53aa2cd6&t=Frozen",
-  });
+  $("form").on("submit", event => {
+    event.preventDefault();
 
-  promise.then(
-    data => {
-      console.log(data);
-    },
-    () => {
-      console.log("bad request");
-    }
-  );
+    const userInput = $('input[type="text"]').val();
+
+    $.ajax({
+      url: "http://www.omdbapi.com/?apikey=53aa2cd6&t=" + userInput,
+    }).then(
+      data => {
+        $("#title").html(data.Title);
+        $("#year").html(data.Year);
+        $("#rated").html(data.Rated);
+      },
+      () => {
+        console.log("bad");
+      }
+    );
+  });
 });
