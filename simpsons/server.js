1674ac8ee069df04bc3263e36f948e4096151f48
Aarik Robles <roblesaarik@gmail.com>
2020-02-08 13:15:00 -0800

    finished simpsons lab

--- simpsons/server.js
+++ simpsons/server.js
@@ -1,3 +1,44 @@
-const express = requires("express");
+const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/Homer", (req, res) => {
+  res.send();
+});
+
+app.get("/Marge", (req, res) => {
+  res.send();
+});
+
+app.get("/Bart", (req, res) => {
+  res.send();
+});
+
+app.get("/Lisa", (req, res) => {
+  res.send();
+});
+
+app.get("/Maggie", (req, res) => {
+  res.send();
+});
+
+app.get("/SnowballII", (req, res) => {
+  res.send();
+});
+
+app.get("/SantasLittleHelper", (req, res) => {
+  res.send(
+    `<html>
+      <body>
+        <h1>Oh ha!</h1>
+      </body>
+    </html>`
+  );
+});
+app.listen(3000, () => {
+  console.log("listening");
+});
