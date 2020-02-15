414699582ad6dd8ababbcb659fa6c820a915c400
Aarik Robles <roblesaarik@gmail.com>
2020-02-15 08:52:05 -0800

    updated new fruit page

--- fruits/server.js
+++ fruits/server.js
@@ -4,30 +4,64 @@ const app = express();
 
 const port = 3000;
 
-const fruits = [
-  {
-    name: "apple",
-    color: "red",
-    readyToEat: true,
-  },
-  {
-    name: "pear",
-    color: "green",
-    readyToEat: false,
-  },
-  {
-    name: "banana",
-    color: "yellow",
-    readyToEat: true,
-  },
-];
+const fruits = require("./models/fruits.js");
+
+/*
+Create = Post
+Read = Get
+Update = Put
+Delete = Delete
+
+request goes in 
+  in between you might want/need to do something
+  you use MIDDLEWARE to do that
+response comes out,
+*/
+
+// Middleware should be placed at the top or your server
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+
+// without this middleware code is undefined
+// with it, it adds data to the key and
+// and sends to the body
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/fruits/", (req, res) => {
   res.send(fruits);
 });
 
+app.get("/fruits/index", (req, res) => {
+  // res.send(fruits[req.params.indexOfFruitsArray]);
+  res.render("index.ejs", {
+    fruit: fruits,
+  });
+});
+
+app.get("/fruits/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-  res.send(fruits[req.params.indexOfFruitsArray]);
+  // res.send(fruits[req.params.indexOfFruitsArray]);
+  res.render("show.ejs", {
+    fruit: fruits[req.params.indexOfFruitsArray],
+  });
+});
+
+// Without this route we get a Cannot POST error
+app.post("/fruits", (req, res) => {
+  console.log(req.body);
+  if (req.body.readyToEat === "on") {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  fruits.push(req.body);
+  console.log("fruits", fruits);
+  res.send("data recieved");
 });
 
 app.listen(port, () => {
