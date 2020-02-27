11dee0d43c1968b653ec230a3652f1869696767f
Aarik Robles <roblesaarik@gmail.com>
2020-02-26 18:49:41 -0800

    setup new page for capitains log

---
+++ capitans_log/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/logs/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
