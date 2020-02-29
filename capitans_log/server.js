5223fd6fe0bbbd8a8e5579ac966463009862f4ca
Aarik Robles <roblesaarik@gmail.com>
2020-02-29 09:55:05 -0800

    created create route

--- capitans_log/server.js
+++ capitans_log/server.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+// NEW ROUTE
 app.get("/logs/new", (req, res) => {
   res.render("new.ejs");
+});
+
+// Create Route
+app.post("/logs", (req, res) => {
+  if (req.body.shipIsBroken === "on") {
+    req.body.shipIsBroken = true;
+  } else {
+    req.body.shipIsBroken = false;
+  }
+  res.send(req.body);
 });
 
 app.listen(3000, () => {
