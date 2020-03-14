8c19309825069e9db19717984d287aa02831791f
Aarik Robles <roblesaarik@gmail.com>
2020-03-14 12:32:06 -0700

    setup Tic Tac Toe

--- react_tac_toe/app.js
+++ react_tac_toe/app.js
@@ -1,3 +1,19 @@
+class Board extends React.Component {
+  render() {
+    return <div>the board!</div>;
+  }
+}
+class Player extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> Player {this.props.whichPlayer} </h2>
+        <h3>Wins: </h3>
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return <h1>React Tac Toe</h1>;
@@ -9,6 +25,8 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        <Player whichPlayer="X" />
+        <Player whichPlayer="O" />
         <p>Heyo! Play soe TTT</p>
       </div>
     );
